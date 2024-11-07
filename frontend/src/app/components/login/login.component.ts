55b140db26f1656eda369403ec0e45474b090ac4
guxttavo <gustavowbarbosas@gmail.com>
2024-11-07 08:19:21 -0300

    estrutura inicial das pastas

---
+++ frontend/src/app/components/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
+// import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+}
