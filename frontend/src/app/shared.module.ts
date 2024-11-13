87b860c2f0ef5b1883a6a4a8d203c8515c75f617
guxttavo <gustavowbarbosas@gmail.com>
2024-11-13 16:14:38 -0300

    validação de nome funcionando

--- frontend/src/app/shared.module.ts
+++ frontend/src/app/shared.module.ts
@@ -4,6 +4,7 @@ import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
     imports: [
@@ -11,14 +12,16 @@ import { ReactiveFormsModule } from '@angular/forms';
         InputTextModule,
         ButtonModule,
         RouterModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        CommonModule
     ],
     exports: [
         CardModule,
         InputTextModule,
         ButtonModule,
         RouterModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        CommonModule
     ]
 })
 export class SharedModule { }
