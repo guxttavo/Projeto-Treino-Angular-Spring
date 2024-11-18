e410ca0067ce4420799b434c775a28763f6d81b9
guxttavo <gustavowbarbosas@gmail.com>
2024-11-18 16:10:39 -0300

    token service criado

--- backend/src/main/java/api/backend/Services/UsuarioService.java
+++ backend/src/main/java/api/backend/Services/UsuarioService.java
@@ -5,8 +5,6 @@ import api.backend.Repositories.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 public class UsuarioService {
 
