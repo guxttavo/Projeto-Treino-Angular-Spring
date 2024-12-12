a34a7a7b3fd3e63e2c8973b803da8cd20c685769
guxttavo <gustavowbarbosas@gmail.com>
2024-12-12 11:01:10 -0300

    remoção pasta API

--- API/src/main/java/api/projeto/API/Repositories/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package api.projeto.API.Repositories;
-
-import api.projeto.API.Entities.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
